6168e16d4a9a2c3fef8cbf3058036d71a3d024ca
Jayaditya <96417285+jay6909@users.noreply.github.com>
2026-04-16 02:07:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ const lib = koffi.load(libPath);
 
 
 const Verify = lib.func('int Verify(const char *)');
+const VerifyBatch = lib.func('const char * VerifyBatch(const char *)');
 
 function verifyEmail(email) {
   return Verify(email);
 }
+// 2. Batch verification
+function verifyEmailsBatch(emails) {
+  // Convert JS array to JSON string for Go
+  const jsonInput = JSON.stringify(emails);
+  
+  // Call the Go native function
+  const jsonResult = VerifyBatch(jsonInput);
+  
+  // Parse the result back to a JS array
+  return JSON.parse(jsonResult);
+}
 
 module.exports = {
-  verifyEmail
+  verifyEmail,verifyEmailsBatch
 };
